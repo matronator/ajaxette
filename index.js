169cdf776d7f5b086edb0b432f0685528decec94
Matronator <5470780+matronator@users.noreply.github.com>
2021-06-17 01:18:58 +0200

    update

--- index.js
+++ index.js
@@ -5,6 +5,15 @@
 * https://opensource.org/licenses/MIT
 */
 
+const hooks = {
+    onAjax: [],
+    onInit: [],
+}
+
+/**
+ * Register AJAX handlers. Currently supports links and forms (`a` and `form` tags).
+ * @param {String} ajaxClass CSS class used for AJAX links (default: `ajax`)
+ */
 function init(ajaxClass = `ajax`) {
     const links = document.querySelectorAll(`a.${ajaxClass}`)
     if (links) {
@@ -15,8 +24,36 @@ function init(ajaxClass = `ajax`) {
             })
         })
     }
+    const forms = document.querySelectorAll(`form.${ajaxClass}`)
+    if (forms) {
+        forms.forEach(form => {
+            form.addEventListener(`submit`, e => {
+                e.preventDefault()
+                const formData = new FormData(form)
+                const params = (new URLSearchParams(formData)).toString()
+                if (form.method.toLowerCase() === `post`) {
+                    handleNetteResponse(form.action, params, `application/x-www-form-urlencoded`)
+                        .catch(err => console.error(err))
+                } else {
+                    handleNetteResponse(`${form.action}?${params}`)
+                        .catch(err => console.error(err))
+                }
+            })
+        })
+    }
+    hooks.onInit.forEach(hook => {
+        if (hook.fn instanceof Function) {
+            hook.fn(...hook.args)
+        }
+    })
 }
 
+/**
+ * Update snippets with data from the response object or redirect to a new URL if set in the response.
+ * @param {String} link URL to call
+ * @param {*} data HTTP Request body
+ * @param {String} contentType Content-Type header (default: `application/json`)
+ */
 async function onAjax(link, data, contentType = `application/json`) {
     const response = await fetch(link, {
         method: data ? `POST` : `GET`,
@@ -35,8 +72,44 @@ async function onAjax(link, data, contentType = `application/json`) {
         if (elem) elem.innerHTML = html
     })
     init()
+
+    hooks.onAjax.forEach(hook => {
+        if (hook.fn instanceof Function) {
+            hook.fn(...hook.args)
+        }
+    })
 }
 
-const ajaxette = { init, onAjax }
+/**
+ * Add function to be executed after onAjax event.
+ * @param {Function} callback Function to execute
+ * @param  {...any} args Function arguments
+ */
+function onAjaxHook(callback, ...args) {
+    onHook(`onAjax`, callback, args)
+}
+
+/**
+ * Add function to be executed after onInit event.
+ * @param {Function} callback Function to execute
+ * @param  {...any} args Function arguments
+ */
+function onInitHook(callback, ...args) {
+    onHook(`onInit`, callback, args)
+}
+
+/**
+ * Add function to be executed after an event.
+ * @param {String} event Name of the event to hook
+ * @param {*} callback Function to execute
+ * @param  {...any} args Function arguments
+ */
+function onHook(event, callback, ...args) {
+    if (hooks[event] instanceof Array) {
+        hooks[event].push({ fn: callback, args: args })
+    }
+}
+
+const ajaxette = { init, onAjax, hooks, onAjaxHook, onInitHook, onHook }
 
 module.exports = ajaxette
--- index.mjs
+++ index.mjs
@@ -5,7 +5,16 @@
 * https://opensource.org/licenses/MIT
 */
 
-export function init(ajaxClass = `ajax`) {
+export const hooks = {
+    onAjax: [],
+    onInit: [],
+}
+
+/**
+ * Register AJAX handlers. Currently supports links and forms (`a` and `form` tags).
+ * @param {String} ajaxClass CSS class used for AJAX links (default: `ajax`)
+ */
+ export function init(ajaxClass = `ajax`) {
     const links = document.querySelectorAll(`a.${ajaxClass}`)
     if (links) {
         links.forEach(link => {
@@ -15,8 +24,37 @@ export function init(ajaxClass = `ajax`) {
             })
         })
     }
+
+    const forms = document.querySelectorAll(`form.${ajaxClass}`)
+    if (forms) {
+        forms.forEach(form => {
+            form.addEventListener(`submit`, e => {
+                e.preventDefault()
+                const formData = new FormData(form)
+                const params = (new URLSearchParams(formData)).toString()
+                if (form.method.toLowerCase() === `post`) {
+                    handleNetteResponse(form.action, params, `application/x-www-form-urlencoded`)
+                        .catch(err => console.error(err))
+                } else {
+                    handleNetteResponse(`${form.action}?${params}`)
+                        .catch(err => console.error(err))
+                }
+            })
+        })
+    }
+    hooks.onInit.forEach(hook => {
+        if (hook.fn instanceof Function) {
+            hook.fn(...hook.args)
+        }
+    })
 }
 
+/**
+ * Update snippets with data from the response object or redirect to a new URL if set in the response.
+ * @param {String} link URL to call
+ * @param {*} data HTTP Request body
+ * @param {String} contentType Content-Type header (default: `application/json`)
+ */
 export async function onAjax(link, data, contentType = `application/json`) {
     const response = await fetch(link, {
         method: data ? `POST` : `GET`,
@@ -35,4 +73,44 @@ export async function onAjax(link, data, contentType = `application/json`) {
         if (elem) elem.innerHTML = html
     })
     init()
+
+    hooks.onAjax.forEach(hook => {
+        if (hook.fn instanceof Function) {
+            hook.fn(...hook.args)
+        }
+    })
 }
+
+/**
+ * Add function to be executed after onAjax event.
+ * @param {Function} callback Function to execute
+ * @param  {...any} args Function arguments
+ */
+export function onAjaxHook(callback, ...args) {
+    onHook(`onAjax`, callback, args)
+}
+
+/**
+ * Add function to be executed after onInit event.
+ * @param {Function} callback Function to execute
+ * @param  {...any} args Function arguments
+ */
+export function onInitHook(callback, ...args) {
+    onHook(`onInit`, callback, args)
+}
+
+/**
+ * Add function to be executed after an event.
+ * @param {String} event Name of the event to hook
+ * @param {*} callback Function to execute
+ * @param  {...any} args Function arguments
+ */
+export function onHook(event, callback, ...args) {
+    if (hooks[event] instanceof Array) {
+        hooks[event].push({ fn: callback, args: args })
+    }
+}
+
+const ajaxette = { init, onAjax, hooks, onAjaxHook, onInitHook, onHook }
+
+export default ajaxette
